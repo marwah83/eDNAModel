dbefe2c982afc6b21265530e97806946cb63f880
marwah83 <marwah.soliman@ntnu.no>
2025-03-24 10:01:56 +0100

    Update phylosq_function_documented.R

--- R/phylosq_function_documented.R
+++ R/phylosq_function_documented.R
@@ -9,7 +9,7 @@
 #'
 #' @examples
 #' \dontrun{
-#' data_array0 <- data_array_phyloseq("Data/longdataexample.RDS")
+#' data_array <- data_array_phyloseq("~/path/to/phyloseq_object.RDS")
 #' }
 #'
 #' @export

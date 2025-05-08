b001bc09cffd64e99392ef1fe8f270d98afe5dfc
marwah83 <marwah.soliman@ntnu.no>
2025-05-08 11:21:25 +0200

    Update fitted_values.R

--- R/fitted_values.R
+++ R/fitted_values.R
@@ -12,10 +12,21 @@ fitted_TMB <- function(model) {
   # Abundance component: etaa = log(lambda) => apply exp()
   fitted_abundance <- exp(rep$etaa)
 
-  list(
+  # Assign proper species names if available
+  species_names <- colnames(model$TMBobj$env$data$Y)
+  if (!is.null(species_names)) {
+    colnames(fitted_abundance) <- species_names
+  }
+
+  species_names_occ <- colnames(model$TMBobj$env$data$Ysites)
+  if (!is.null(species_names_occ)) {
+    colnames(fitted_occupancy) <- species_names_occ
+  }
+
+  return(list(
     fitted_occupancy = as.matrix(fitted_occupancy),
     fitted_abundance = as.matrix(fitted_abundance)
-  )
+  ))
 }
 
 

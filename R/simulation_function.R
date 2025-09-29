33bbe036273f73518c652ff4e698141237ea9724
Marwah Soliman <marwah.soliman@ntnu.no>
2025-09-29 15:36:44 +0200

    remove not used

--- R/simulation_function.R
+++
@@ -1,8 +0,0 @@
-# ✅ Run Simulation Over Parameter Grid
-start_time <- Sys.time()
-
-simulation_results <- do.call(rbind, lapply(1:nrow(param_grid), function(i) {
-  with(param_grid[i, ], run_full_simulation(Species, Sites, Replicates, Lambda, ZIP))
-}))
-
-end_time <- Sys.time()
--- tests/testthat/test-bias.R
+++
@@ -1,6 +0,0 @@
-test_that("compute_bias calculates correctly", {
-  actual <- c(1, 2, 3, 4, 5)
-  estimated <- c(1.1, 2.2, 2.9, 4.1, 5.0)
-  expected_bias <- mean(estimated - actual)
-  expect_equal(compute_bias(actual, estimated), expected_bias, tolerance = 1e-6)
-})

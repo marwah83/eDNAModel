81660b7284bc4c70d75bd1889c527005ca32eab8
marwah83 <marwah.soliman@ntnu.no>
2026-05-07 16:25:09 +0200

    Update and rename predict.R to cv_predict.R

--- R/cv_predict.R
+++ R/cv_predict.R
@@ -1,15 +1,20 @@
+#' Predict from FitModel object
+#'
+#' @param fit Output from FitModel
+#' @param newdata Data frame for prediction
+#'
+#' @return Data frame with predictions
+#' @export
 predict_FitModel <- function(fit, newdata) {
 
-  # Merge predictions with newdata keys
   pred <- fit$lambda
 
   keys <- intersect(names(pred), names(newdata))
 
   out <- dplyr::left_join(newdata, pred, by = keys)
 
-  # fallback if missing
   if (!("lambda_mean" %in% names(out))) {
-    stop("lambda_mean missing in predictions")
+    stop("lambda_mean missing in predictions.")
   }
 
   out

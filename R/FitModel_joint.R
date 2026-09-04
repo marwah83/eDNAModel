ee4cc1653e5fd5b5dcbced48980bea5786d6e59a
marwah83 <marwah.soliman@ntnu.no>
2026-09-04 13:56:37 +0200

    Update FitModel_joint.R

--- R/FitModel_joint.R
+++ R/FitModel_joint.R
@@ -426,7 +426,7 @@ FitModel_joint <- function(
     # FALSE is deliberately the default for report covariance.
     # ----------------------------------------------------------
     get_report_covariance = FALSE,
-    get_joint_precision = TRUE,
+    get_joint_precision = FALSE,
 
     # ----------------------------------------------------------
     # Heuristic parameter diagnostics
